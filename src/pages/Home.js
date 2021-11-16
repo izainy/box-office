410a794259f7d33ad9bd37c38a92b96b71a4f2bc
izainy <zainabisrar99@gmail.com>
2021-11-16 23:10:04 +0530

    created show page components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   const [searchOption, setSearchOption] = useState('shows');
 
   const isShowsSearch = searchOption == 'shows';
+
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
